dab65442f0b03d8a2d19e297843296bec075d921
FlavioGomes7 <flavio.gomes@estudante.ifms.edu.br>
2023-04-10 18:46:29 -0400

    Implementação Parcial do Sistema de Itens

---
+++ Assets/Scripts/Scene_Object.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjectSO", menuName = "SceneObject" )]
+
+public class Scene_Object : ScriptableObject
+{
+    public Mesh model;
+    private string[] password;
+    public bool interactive;
+
+
+
+  
+}
