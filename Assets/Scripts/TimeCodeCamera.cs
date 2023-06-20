cbeedb69039642734c9bc6a4da664b88f2b98d9c
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-06-20 17:52:29 -0400

    VHS CAMERA RETRO LAYOUT

---
+++ Assets/Scripts/TimeCodeCamera.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+using TMPro;
+public class TimeCodeCamera : MonoBehaviour
+{
+    public int startHour = 0;
+    public int startMinute = 0;
+    public int startSecond = 0;
+
+    private float elapsedTime = 0f;
+    private int currentHour = 0;
+    private int currentMinute = 0;
+    private int currentSecond = 0;
+    TextMeshProUGUI textTimer;
+    private void Start()
+    {
+        textTimer = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        // Calcula o tempo atual com base no tempo decorrido desde o início
+        currentHour = startHour + Mathf.FloorToInt(elapsedTime / 3600);
+        currentMinute = startMinute + Mathf.FloorToInt(elapsedTime / 60) % 60;
+        currentSecond = startSecond + Mathf.FloorToInt(elapsedTime % 60);
+
+        // Exibe o time code no console
+        textTimer.SetText("{0}:{1}:{2}", currentHour, currentMinute, currentSecond);
+    }
+
+}
